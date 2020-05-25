c6c43ef98d07df7da44fb80357a191666e474342
Shravan Mechineni <shravanmechineni5@gmail.com>
2020-05-25 15:32:41 +0100

    webhook test

--- src/test/java/com/devopsdemo/utilities/TestLogger.java
+++ src/test/java/com/devopsdemo/utilities/TestLogger.java
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory;
 
 public class TestLogger {
 
+	
         private static final Logger LOG = LoggerFactory.getLogger(TestLogger.class);
         @Test
         public void testGetErrorMessage1() {
